002c3ec02fd688ae3c0c95b24094b16af6ecaa8f
monsieurbadia <contact@monsieurbadia.com>
2019-07-28 17:57:04 +0200

    MB: Add Credits Module

---
+++ src/components/Common/Credits/Credits.js
@@ -0,0 +1,48 @@
+import { useEffect, useRef } from 'react';
+
+const bros = [
+  [ 'coding by @monsieurbadia', 'https://monsieurbadia.com' ]
+];
+
+const Credits = () => {
+
+  const author = useRef( 'Bedroom Galaxy | Coding by @monsieurbadia' );
+  const website = useRef( 'https://monsieurbadia.com' );
+
+  useEffect( () => {
+
+    show();
+
+  });
+
+  const show = () => {
+
+    if ( navigator.userAgent.toLowerCase().indexOf( 'chrome' ) > - 1 ) {
+
+      for ( let index = bros.reverse().length - 1; index >= 0; index -- ) {
+
+        const [ author, website ] = bros[ index ];
+
+        const args = [
+          `%c ${ author } %c ${ website }`,
+          `color: #ffffff; background: #ff5c7c; padding: 3px 0;`,
+          `padding: 3px 1px; color: #000000; text-decoration: none;`,
+        ];
+
+        console.log.apply( console, args );
+
+      };
+    
+    } else if ( window.console ) {
+    
+      console.log( `${ author.current } ${ website.current } `);
+    
+    }
+  
+  }
+
+  return null;
+
+};
+
+export default Credits;
