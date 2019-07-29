e61a50f273bc5cb7de9e43c5b1c4726a9a5c8c11
monsieurbadia <contact@monsieurbadia.com>
2019-07-29 02:26:51 +0200

    MB: Add monsieurbadia Logo SVG

---
+++ src/components/Common/Loading/LoadingRenderer/LoadingRendererSVG/LoadingRendererSVG.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Loading () {
+
+  return (
+
+    <div className='loading-renderer loading-renderer--svg'>
+      <h3 className='loading-renderer-svg-title'>chargement</h3>
+    </div>
+
+  );
+
+};
--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -55,21 +55,188 @@ export default withRouter( function Home () {
 
       { isLoading ? (
 
-        <div className='loading-renderer-html'>
-
-          <h1 className='loading-renderer-html-title'>
-            <div className='loading-renderer-html-title-slot'>monsieurbadia</div>
-          </h1>
-
+        <div className='loading-renderer-svg'>
           <svg
-            clipRule="evenodd"
-            fillRule="evenodd"
-            width="100"
-            viewBox="0 200 845 425"
+            className="monsieurbadia-logo-svg"
+            width="200"
+            height="200"
+            style={ {
+              'cursor' : 'pointer',
+              'userSelect' : 'none'
+            } }
+            viewBox="0 0 800 850"
             xmlns="http://www.w3.org/2000/svg">
-            <path fill="#000000" d="M4 579l82 0 251 -333 130 170 84 -111 210 274 82 0 0 22 -839 0 0 -22zm109 0l354 0 -134 -291 -220 291zm378 0l74 0 -186 -243 112 243zm101 0l61 0 -107 -232 -65 87 111 145zm85 0l56 0 -140 -184 84 184z" />
+            <g className="monsieurbadia-logo-mister-no-face">
+              <g className="monsieurbadia-logo-mister-no-face-body">
+                <g className="monsieurbadia-logo-mister-no-face-body-neck">
+                  <path
+                    className="animated_0"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='442 444'
+                    d="M338.6,407.2L339.7,486.4L400.1,543.4L457.5,486.4L457.5,407.2Z" />
+                </g>
+                <g className="monsieurbadia-logo-mister-no-face-body-collar">
+                  <path
+                    className="animated_1"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='237 239'
+                    d="M339.5,469.1L295.1,518.7L356.2,597.1L400.1,542.1" />
+                  <path
+                    className="animated_2"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='242 244'
+                    d="M458,464.6L505.6,518.7L444.5,597.1L400.6,542.1" />
+                  <path
+                    className="animated_3"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='245 247'
+                    d="M400.1,542.1L400.1,787.1" />
+                </g>
+                <g className="monsieurbadia-logo-mister-no-face-body-left-hand">
+                  <path
+                    className="animated_4"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='332 334'
+                    d="M295.1,518.7L195.9,565.4L205.9,787.1" />
+                  <path
+                    className="animated_5"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='252 254'
+                    d="M218.7,554.7L277.5,637.4L277.5,787.1" />
+                </g>
+                <g className="monsieurbadia-logo-mister-no-face-body-right-hand">
+                  <path
+                    className="animated_6"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='332 334'
+                    d="M506,518.7L605.3,565.4L595.2,787.1" />
+                  <path
+                    className="animated_7"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='252 254'
+                    d="M585,554.7L526.2,637.4L526.2,787.1" />
+                </g>
+                <g className="monsieurbadia-logo-mister-no-face-body-pocket">
+                  <path
+                    className="animated_8"
+                    fill="none"
+                    stroke="#000000"
+                    strokeWidth="3"
+                    strokeMiterlimit="10"
+                    strokeDashoffset='0'
+                    strokeDasharray='53 55'
+                    d="M314.6,661.1L367.4,661.1" />
+                </g>
+              </g>
+              <g className="monsieurbadia-logo-mister-no-face-body-head">
+                <g className="monsieurbadia-logo-mister-no-face-body-head-structure">
+                  <path
+                    className="animated_9"
+                    fill="#1d1d1b"
+                      strokeDashoffset='0'
+                      strokeDasharray='1046 1048'
+                    d="M400.6,502.5L154.1,256.1L400.6,9.6L647,256.1" />
+                </g>
+                <g className="monsieurbadia-logo-mister-no-face-body-head-eye">
+                  <g className="monsieurbadia-logo-mister-no-face-body-head-eye-structure">
+                    <path
+                      className="animated_10"
+                      fill="#ffffff"
+                      stroke="#000000"
+                      strokeMiterlimit="10"
+                      strokeDashoffset='0'
+                      strokeDasharray='437 439'
+                      d="M400.6,211.4L300.9,253.5L400.6,300.7L500.2,253.5Z" />
+                  </g>
+                  <g className="monsieurbadia-logo-mister-no-face-body-head-eye-iris">
+                    <path
+                      className="animated_11"
+                      fill="none"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeMiterlimit="10"
+                      strokeDashoffset='0'
+                      strokeDasharray='123 125'
+                      d="M364.6,308c-17.1-11.6-28.4-31.2-28.4-53.4c0-21.2,10.2-40,26-51.7" />
+                    <path
+                      className="animated_12"
+                      fill="none"
+                      stroke="#1D1D1B"
+                      strokeWidth="2"
+                      strokeMiterlimit="10"
+                      strokeDashoffset='0'
+                      strokeDasharray='120 122'
+                      d="M443.7,206.8c13.1,11.8,21.3,28.8,21.3,47.8c0,23.1-12.1,43.3-30.4,54.7" />
+                  </g>
+                  <g className="monsieurbadia-logo-mister-no-face-body-head-eye-pupil">
+                    <path
+                      className="animated_13"
+                      fill="#000000"
+                      strokeDashoffset='0'
+                      strokeDasharray='133 135'
+                      d="M379.5,254.6A21.1,21.1 0,1,1 421.70000000000005,254.6A21.1,21.1 0,1,1 379.5,254.6" />
+                    <g className="eye-pupil-glow">
+                      <path
+                        className="animated_14"
+                        fill="#ffffff"
+                        strokeDashoffset='0'
+                        strokeDasharray='31 33'
+                        d="M403.2,257.1A4.8,4.8 0,1,1 412.8,257.1A4.8,4.8 0,1,1 403.2,257.1" />
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <g className='monsieurbadia-logo-mister-no-face-title'>
+              <text
+                x="50%"
+                y="850"
+                fill="#000000"
+                style={ {
+                  'fontFamily': 'sans-serif',
+                  'fontSize': '50px',
+                  'fontWeight': '400',
+                  'textAnchor': 'middle',
+                  'textTransform': 'uppercase',
+                  'whiteSpace': 'pre-line',
+                  'letterSpacing': '0px'
+                } }>
+                monsieurbadia
+              </text>
+            </g>
           </svg>
-
         </div>
 
       ) : (
